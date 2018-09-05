633540bbdd8c5f703fa19c1f6b28d979fea587b4
hangyang.lin <HangYang.Lin@geely.com>
2018-09-05 14:23:26 +0800

    删除target

--- src/main/java/com/linable/helloworld/LinableApplication.java
+++ src/main/java/com/linable/helloworld/LinableApplication.java
@@ -12,6 +12,7 @@ public class LinableApplication {
 
     public static void main(String[] args) throws Exception {
         SpringApplication application = new SpringApplication(LinableApplication.class);
+		String a = "a";
         application.run(args);
     }
 
